3fb1a880cdf4472cd26000a4bf2e62496e4071a5
Cesar993 <ce.serrano@duocuc.cl.com>
2023-10-10 01:17:53 -0300

    Cfunda

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+/* 1 */
+ for (int i = 1; i < 256; i++)
+{
+    Console.WriteLine(i);
+} 
+
+/* 2 */
+for (int i = 1; i < 101; i++)
+ {
+    if (i%3==0 && i%5==0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i%5==0)
+    {
+          Console.WriteLine("Buzz");
+    }
+    else if (i%3==0)
+    {
+          Console.WriteLine("Fizz");
+    }
+    else
+    {
+        
+     Console.WriteLine(i);
+    }
+} 
+
+/* 3 */
+int j =1;
+while (j< 101)
+{
+    if (j%3==0 && j%5==0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (j%5==0)
+    {
+          Console.WriteLine("Buzz");
+    }
+    else if (j%3==0)
+    {
+          Console.WriteLine("Fizz");
+    }
+    else
+    {
+        
+     Console.WriteLine(j);
+    }
+
+    j++;
+}
